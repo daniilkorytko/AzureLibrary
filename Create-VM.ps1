c38f0eb17b844447e4bcc658bfe00a71bcf276a6
daniilkorytko <43876618+daniilkorytko@users.noreply.github.com>
2018-12-14 18:07:06 +0300

    Add files via upload

--- Create-VM.ps1
+++ Create-VM.ps1
@@ -1,25 +1,9 @@
-$ResourceGroup = "DaniilK"
-$MyLOcation = "westeurope"
-$adminUsername = 'Administrator1'
-$Secure_String_Pwd = ConvertTo-SecureString "Administrator1" -AsPlainText -Force
+﻿$ResourceGroup = "DaniilK"
+$LOcation = "westeurope"
 
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $LOcation -Force
 
-$TemplateParametersCreateVM = "https://raw.githubusercontent.com/daniilkorytko/AzureLibrary/master/Create-ResourceVMParametrs.json"
-$TemplateUriCreateVM = "https://raw.githubusercontent.com/daniilkorytko/AzureLibrary/master/Create-ResourceVM.json"
-
-
-$FileParametersCreateVM = "$env:TEMP\FileParametersCreateVM.json"
-$FileCreateVM  = "$env:TEMP\FileCreateVM.json"
-
-
-Invoke-WebRequest -Uri $TemplateParametersCreateVM -OutFile $FileParametersCreateVM
-Invoke-WebRequest -Uri $TemplateUriCreateVM -OutFile $FileCreateVM
-
-
-New-AzureRmResourceGroupDeployment  -ResourceGroupName $ResourceGroup `
-   -TemplateFile $FileCreateVM `
-   -TemplateParameterFile $FileParametersCreateVM `
-   -AdminPassword $Secure_String_Pwd -adminUsername $adminUsername -Verbose `
-   -virtualMachineName 'DaniilK-VM1' 
-
+$deployment = New-AzureRmResourceGroupDeployment  -ResourceGroupName $ResourceGroup `
+   -TemplateFile "C:\Work\Azure Part 2\Create-VM\OnlyARM\Create-ResourceVM.jsonc" `
+   -TemplateParameterFile "C:\Work\Azure Part 2\Create-VM\OnlyARM\Create-ResourceVMParametrs.json" -Verbose
 
